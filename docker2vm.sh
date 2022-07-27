edd1e4078ca8837ae7a8803b16cfe2e9aea476a4
Aurelien Esnard <aurelien.esnard@u-bordeaux.fr>
2022-07-27 13:15:12 +0200

    init import

---
+++ docker2vm.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+[ ! $# -eq 2 ] && echo "Usage: $0 <docker> <vm.img>" && exit 1
+
+DOCKERIMG="$1"
+VMIMG="$2"
+
+set -e # immediately exit on first error
+
+echo_blue() {
+    local font_blue="\033[94m"
+    local font_bold="\033[1m"
+    local font_end="\033[0m"
+    echo -e "\n${font_blue}${font_bold}${1}${font_end}"
+}
+
+echo_blue "[Export docker as a tarball]"
+DOCKERCID=$(docker run -d $DOCKERIMG /bin/true)
+(set -x ; docker export -o $VMIMG.tar ${DOCKERCID})
+
+echo_blue "[Make a disk image from the tarball]"
+(set -x ; virt-make-fs --type=ext3 --partition=mbr --format=raw --size=+2G $VMIMG.tar $VMIMG)
+unlink $VMIMG.tar
+
+echo_blue "[Switch on the bootable flag]"
+(set -x ; sfdisk --activate $VMIMG 1)
+
+echo_blue "[Write MBR]"
+(set -x ; dd if=/usr/lib/syslinux/mbr/mbr.bin of=$VMIMG bs=440 count=1 conv=notrunc)
+
+echo_blue "[List partitions]"
+(set -x ; sfdisk -l $VMIMG)
+
+echo_blue "[Install extlinux bootloader]"
+(set -x ; guestfish --rw -a $VMIMG -i extlinux /boot)
+(set -x ; virt-copy-in -a $VMIMG syslinux.cfg /boot/)
+
+# echo_blue "[Convert to qcow2]"
+# qemu-img convert -c $VMIMG -O qcow2 $VMIMG.qcow2
+
+echo_blue "Done!"
